28013fb25d58bddd755fa0557454ffeba3d1c374
sriramuluramadevi3-wq <sriramuluramadevi3@gmail.com>
2026-06-12 19:25:48 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Servlets;
+
+/**
+ * 
+ */
+public class LoginServlet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
